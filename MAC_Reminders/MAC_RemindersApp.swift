0ee3fc66d52533eebd24d4290b7eb419fcfa554f
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-11-07 15:45:48 +0100

    Initial Commit

---
+++ MAC_Reminders/MAC_RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAC_RemindersApp.swift
+//  MAC_Reminders
+//
+//  Created by Miguel Gallego on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MAC_RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
